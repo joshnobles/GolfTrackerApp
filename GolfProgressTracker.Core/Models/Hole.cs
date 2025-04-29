2dfb60cc06c9d9fcef8d926da2a7a1efe44545c0
Josh Nobles <noblesj002@gmail.com>
2025-04-28 22:24:06 -0400

    Add project files.

---
+++ GolfProgressTracker.Core/Models/Hole.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GolfProgressTracker.Core.Models
+{
+    [Table("Hole")]
+    class Hole
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int RoundId { get; set; }
+
+        public int Par { get; set; }
+
+        public int Shots { get; set; }
+    }
+}
---
+++ GolfProgressTracker.Core/Models/Round.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GolfProgressTracker.Core.Models
+{
+    [Table("Round")]
+    class Round
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; } = null!;
+    }
+}
