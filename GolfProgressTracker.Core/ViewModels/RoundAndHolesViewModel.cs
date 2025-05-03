6a659ca410ebda0a6ba962638e3afebd31e6568f
Josh Nobles <noblesj002@gmail.com>
2025-05-03 15:59:38 -0400

    Fixed bug where default DatePlayed did not show up as the current date.

--- GolfProgressTracker.Core/ViewModels/RoundAndHolesViewModel.cs
+++ GolfProgressTracker.Core/ViewModels/RoundAndHolesViewModel.cs
@@ -1,10 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace GolfProgressTracker.Core.ViewModels
+﻿namespace GolfProgressTracker.Core.ViewModels
 {
     public class RoundAndHolesViewModel
     {
-        public RoundViewModel Round { get; set; } = null!;
+        public RoundViewModel Round { get; set; } = new();
 
         public List<HoleViewModel> Holes { get; set; } = [];
     }
